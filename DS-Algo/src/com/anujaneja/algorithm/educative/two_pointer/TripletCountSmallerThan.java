740608d86fa9515b0b8f6f79e917e01201957a05
Anuj Aneja <anuj.ymca@gmail.com>
2021-01-04 18:36:46 +0530

    Two pointer pattern : Triplet sum smaller than target - test cases added.

--- DS-Algo/src/com/anujaneja/algorithm/educative/two_pointer/TripletCountSmallerThan.java
+++ DS-Algo/src/com/anujaneja/algorithm/educative/two_pointer/TripletCountSmallerThan.java
@@ -35,42 +35,58 @@ public class TripletCountSmallerThan {
         //Test Case: 1 +ve
         int[] nums = new int[]{-1,0,2,3};
         int target=3;
-        int testCaseNo=0;
+        int testCaseNo=1;
 
-        System.out.println("Test Case #: "+(testCaseNo++)+" - Count= "+threeCountSmaller(nums,target));
+        System.out.println("Test Case #: "+(testCaseNo)+" - Count= "+threeCountSmaller(nums,target));
 
         //Test Case: 2 +ve
         nums = new int[]{-1,4,2,1,3};
         target=5;
+        testCaseNo++;
 
-        System.out.println("Test Case #: "+(testCaseNo++)+" - Count= "+threeCountSmaller(nums,target));
+        System.out.println("Test Case #: "+(testCaseNo)+" - Count= "+threeCountSmaller(nums,target));
 
         //Test Case: 3 -ve
         nums = new int[]{-1};
         target=5;
+        testCaseNo++;
 
         try{
-            System.out.println("Test Case #: "+(testCaseNo++)+" - Count= "+threeCountSmaller(nums,target));
+            System.out.println("Test Case #: "+(testCaseNo)+" - Count= "+threeCountSmaller(nums,target));
         } catch (IllegalArgumentException ex) {
-            System.out.println("Test case #: "+(testCaseNo++)+" - Exception: "+ex.getMessage()+" for "+Arrays.toString(nums));
+            System.out.println("Test case #: "+(testCaseNo)+" - Exception: "+ex.getMessage()+" for "+Arrays.toString(nums));
         }
 
 
         //Test Case: 4 -ve
         nums = new int[]{-5,5};
         target=5;
+        testCaseNo++;
 
         try{
-            System.out.println("Test Case #: "+(testCaseNo++)+" - Count= "+threeCountSmaller(nums,target));
+            System.out.println("Test Case #: "+(testCaseNo)+" - Count= "+threeCountSmaller(nums,target));
         } catch (IllegalArgumentException ex) {
-            System.out.println("Test case #: "+(testCaseNo++)+" - Exception: "+ex.getMessage()+" for "+Arrays.toString(nums));
+            System.out.println("Test case #: "+(testCaseNo)+" - Exception: "+ex.getMessage()+" for "+Arrays.toString(nums));
         }
 
 
         //Test Case: 5 -ve
         nums = new int[]{-5,5,7};
         target=5;
+        testCaseNo++;
 
-        System.out.println("Test Case #: "+(testCaseNo++)+" - Count= "+threeCountSmaller(nums,target));
+        System.out.println("Test Case #: "+(testCaseNo)+" - Count= "+threeCountSmaller(nums,target));
+
+
+        //Test Case: 5 -ve
+        nums = new int[]{};
+        target=5;
+        testCaseNo++;
+
+        try{
+            System.out.println("Test Case #: "+(testCaseNo)+" - Count= "+threeCountSmaller(nums,target));
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Test case #: "+(testCaseNo)+" - Exception: "+ex.getMessage()+" for "+Arrays.toString(nums));
+        }
     }
 }
