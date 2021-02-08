cb7b872f9429037c832738e694d5f6070c404c8a
Anuj Aneja <anuj.ymca@gmail.com>
2021-02-08 23:08:14 +0530

    BST Search Implemented...

--- DS-Algo/src/com/anujaneja/algorithm/tree/BST/BSTSearchIterative.java
+++ DS-Algo/src/com/anujaneja/algorithm/tree/BST/BSTSearchIterative.java
@@ -1,4 +1,41 @@
 package com.anujaneja.algorithm.tree.BST;
 
+import com.anujaneja.algorithm.tree.TreeNode;
+
 public class BSTSearchIterative {
+
+    public static Integer searchBST(TreeNode root,int key) {
+        if(root==null) {
+            return null;
+        }
+
+        TreeNode curr = root;
+
+        while(curr!=null) {
+            if(curr.val==key) {
+                return curr.val;
+            } else if(key<curr.val) {
+                curr=curr.left;
+            } else {
+                curr=curr.right;
+            }
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        //Valid BST...
+        TreeNode root = new TreeNode(8);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(10);
+        root.left.left = new TreeNode(1);
+        Integer key = 3;
+        System.out.println("SearchBST : key= "+key+" found: "+(key.equals(searchBST(root,key))));
+
+        key = 1;
+
+        System.out.println("SearchBST : key= "+key+" found: "+(key.equals(searchBST(root,key))));
+
+    }
 }
--- DS-Algo/src/com/anujaneja/algorithm/tree/BST/BSTSearchRecursive.java
+++ DS-Algo/src/com/anujaneja/algorithm/tree/BST/BSTSearchRecursive.java
@@ -1,4 +1,38 @@
 package com.anujaneja.algorithm.tree.BST;
 
+import com.anujaneja.algorithm.tree.TreeNode;
+
 public class BSTSearchRecursive {
+
+    public static Integer searchBST(TreeNode root,int key) {
+        if(root==null) {
+            return null;
+        }
+
+        if(root.val==key) {
+            return root.val;
+        }
+
+        if(key<root.val) {
+            return searchBST(root.left,key);
+        } else {
+            return searchBST(root.right,key);
+        }
+    }
+
+    public static void main(String[] args) {
+        //Valid BST...
+        TreeNode root = new TreeNode(8);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(10);
+        root.left.left = new TreeNode(1);
+        Integer key = 3;
+        System.out.println("SearchBST : key= "+key+" found: "+(key.equals(searchBST(root,key))));
+
+        key = 11;
+
+        System.out.println("SearchBST : key= "+key+" found: "+(key.equals(searchBST(root,key))));
+
+    }
 }
+
