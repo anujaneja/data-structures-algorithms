1f627613335b4c15f504106ae0f83767f2cfd374
Anuj Aneja <anuj.ymca@gmail.com>
2021-02-09 23:58:29 +0530

    BST Ancestor...

--- DS-Algo/src/com/anujaneja/algorithm/tree/BST/BSTAncestor.java
+++ DS-Algo/src/com/anujaneja/algorithm/tree/BST/BSTAncestor.java
@@ -1,4 +1,41 @@
 package com.anujaneja.algorithm.tree.BST;
 
+import com.anujaneja.algorithm.tree.TreeNode;
+
+import java.util.ArrayList;
+
 public class BSTAncestor {
+    public static void main(String[] args) {
+        int arr[] = { 20, 22, 8, 4, 10, 12, 14 };
+
+        TreeNode root = new TreeNode(arr);
+
+        System.out.println("ancestors==="+ancestors(root,20));
+    }
+
+    public static ArrayList<Integer> ancestors(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode curr = root;
+        ArrayList<Integer> arrayList =  new ArrayList<>();
+
+
+        while (curr != null) {
+            if (key > curr.val) {
+                arrayList.add(curr.val);
+                curr = curr.right;
+            } else if(key < curr.val){
+                arrayList.add(curr.val);
+                curr = curr.left;
+            } else {
+                break;
+            }
+        }
+
+        return arrayList;
+    }
+
+
 }
