ca944534dee0dc0f09f17261e4a343e6e21dc6d7
Anuj Aneja <anuj.ymca@gmail.com>
2021-04-15 23:06:34 +0530

    K larget number using minheap solved...

---
+++ DS-Algo/src/com/anujaneja/algorithm/educative/top_elements/KthSmallestNumber.java
@@ -0,0 +1,34 @@
+package com.anujaneja.algorithm.educative.top_elements;
+
+import java.util.PriorityQueue;
+
+public class KthSmallestNumber {
+    public static int findKthSmallestNumber(int[] nums, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((o1,o2)->o2-o1);
+
+        for(int i=0;i<k;i++) {
+            maxHeap.offer(nums[i]);
+        }
+
+        for(int i=k;i<nums.length;i++) {
+            if(nums[i]<maxHeap.peek()) {
+                maxHeap.poll();
+                maxHeap.offer(nums[i]);
+            }
+        }
+
+        return maxHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        int result = KthSmallestNumber.findKthSmallestNumber(new int[] { 1, 5, 12, 2, 11, 5 }, 3);
+        System.out.println("Kth smallest number is: " + result);
+
+        // since there are two 5s in the input array, our 3rd and 4th smallest numbers should be a '5'
+        result = KthSmallestNumber.findKthSmallestNumber(new int[] { 1, 5, 12, 2, 11, 5 }, 4);
+        System.out.println("Kth smallest number is: " + result);
+
+        result = KthSmallestNumber.findKthSmallestNumber(new int[] { 5, 12, 11, -1, 12 }, 3);
+        System.out.println("Kth smallest number is: " + result);
+    }
+}
