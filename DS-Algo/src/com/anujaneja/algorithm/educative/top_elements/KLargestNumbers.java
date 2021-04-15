32cc1b24ec698419deac6d9911857f0a46dfa5a8
Anuj Aneja <anuj.ymca@gmail.com>
2021-04-15 16:10:01 +0530

    K larget number using minheap solved...

--- DS-Algo/src/com/anujaneja/algorithm/educative/top_elements/KLargestNumbers.java
+++ DS-Algo/src/com/anujaneja/algorithm/educative/top_elements/KLargestNumbers.java
@@ -2,19 +2,31 @@ package com.anujaneja.algorithm.educative.top_elements;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 class KLargestNumbers {
 
-  public static List<Integer> findKLargestNumbers(int[] nums, int k) {
-    // TODO: Write your code here
-    return new ArrayList<>();
-  }
+    public static List<Integer> findKLargestNumbers(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((o1, o2) -> o1 - o2);
 
-  public static void main(String[] args) {
-    List<Integer> result = KLargestNumbers.findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
-    System.out.println("Here are the top K numbers: " + result);
+        for (int i = 0; i < k; i++) {
+            minHeap.offer(nums[i]);
+        }
 
-    result = KLargestNumbers.findKLargestNumbers(new int[] { 5, 12, 11, -1, 12 }, 3);
-    System.out.println("Here are the top K numbers: " + result);
-  }
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.offer(nums[i]);
+            }
+        }
+        return new ArrayList<>(minHeap);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> result = KLargestNumbers.findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+
+        result = KLargestNumbers.findKLargestNumbers(new int[] { 5, 12, 11, -1, 12 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+    }
 }
