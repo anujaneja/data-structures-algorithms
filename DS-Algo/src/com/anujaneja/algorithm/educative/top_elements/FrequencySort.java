5fa7bd3bf8c07a6da4c3e3035597663a097e6240
Anuj Aneja <anuj.ymca@gmail.com>
2021-04-16 19:42:33 +0530

    Frequency sort implemented...- Alternate solution

--- DS-Algo/src/com/anujaneja/algorithm/educative/top_elements/FrequencySort.java
+++ DS-Algo/src/com/anujaneja/algorithm/educative/top_elements/FrequencySort.java
@@ -1,6 +1,8 @@
 package com.anujaneja.algorithm.educative.top_elements;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -34,29 +36,43 @@ class FrequencySort {
 
     /**
      * Alternate solution...
+     *
      * @param S
      * @return
      */
     public static String sortCharacterByFrequencyBucketSort(String S) {
-        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<Map.Entry<Character, Integer>>((o1, o2) -> o2.getValue() - o1.getValue());
         Map<Character, Integer> freqMap = new HashMap<>();
 
         for (int i = 0; i < S.length(); i++) {
             freqMap.put(S.charAt(i), freqMap.getOrDefault(S.charAt(i), 0) + 1);
         }
 
-        maxHeap.addAll(freqMap.entrySet());
-        StringBuilder resultStr = new StringBuilder(S.length());
+        List<Character>[] buckets = new List[S.length() + 1];
 
-        while (!maxHeap.isEmpty()) {
-            Map.Entry<Character, Integer> entry = maxHeap.poll();
+        for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
+            char key = entry.getKey();
+            int freq = entry.getValue();
 
-            for (int i = 0; i < entry.getValue(); i++) {
-                resultStr.append(entry.getKey());
+            if(buckets[freq]==null) {
+                buckets[freq] = new ArrayList<>();
+            }
+
+            buckets[freq].add(key);
+        }
+
+        StringBuilder stringBuilder = new StringBuilder(S.length());
+
+        for(int i=buckets.length-1;i>=0;i--) {
+            if(buckets[i]!=null) {
+                for(char ch:buckets[i]) {
+                    for(int j=0;j<i;j++) {
+                        stringBuilder.append(ch);
+                    }
+                }
             }
         }
 
-        return resultStr.toString();
+        return stringBuilder.toString();
     }
 
     public static void main(String[] args) {
@@ -64,6 +80,13 @@ class FrequencySort {
         System.out.println("Here is the given string after sorting characters by frequency: " + result);
 
         result = FrequencySort.sortCharacterByFrequency("abcbab");
+        System.out.println("Here is the given string after sorting characters by frequency: " + result);
+
+
+        result = FrequencySort.sortCharacterByFrequencyBucketSort("Programming");
+        System.out.println("Here is the given string after sorting characters by frequency: " + result);
+
+        result = FrequencySort.sortCharacterByFrequencyBucketSort("abcbab");
         System.out.println("Here is the given string after sorting characters by frequency: " + result);
     }
 }
